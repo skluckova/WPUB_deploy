270977296c512678e26ea83e8485cec925bd5c4d
Simona Klučková <32175257+skluckova@users.noreply.github.com>
2020-04-24 18:24:26 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,18 @@ module.exports = function(grunt) {
                 src: 'css/blog-home.css',
                 dest: 'css/blog-home.css'
             }
+        },
+        uglify: {
+            dist:{
+                files: {
+                    'script.js':'script.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('default', ['cssmin']);
+    
 }
